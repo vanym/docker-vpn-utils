c2927accfb59c7fc4c4c6c0be9b276b5d53a5cf2
VanyM <vanymdev@gmail.com>
2023-05-20 13:35:30 +0400

    add auth-failure rotation

--- scripts/ovpn-run.sh
+++ scripts/ovpn-run.sh
@@ -10,6 +10,11 @@ ln -sf /proc/1/ns/net /var/run/netns/"$NETNS"
 source /opt/scripts/ovpn-run-rr.sh
 mkdir -p /opt/rr
 rr_rotate /opt/rr/always /etc/openvpn/rr/always.d/
+rr_init /opt/rr/auth-failure /etc/openvpn/rr/auth-failure.d/
+
+auth_failure(){
+  rr_rotate /opt/rr/auth-failure /etc/openvpn/rr/auth-failure.d/
+}
 
 exec openvpn \
   --ifconfig-noexec \
@@ -24,4 +29,7 @@ exec openvpn \
   --down "/opt/scripts/ovpn-hook.sh" \
   --cd /etc/openvpn \
   --config ovpn.conf \
+  --log >(tee > /proc/self/fd/1 \
+            >(grep --line-buffered -o "SIG.*auth-failure.*received" | \
+                while read ; do auth_failure ; done) ) \
   "${@}"
