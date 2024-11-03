902c32c6c665c2d19587314863f0f73ea5871a43
VanyM <vanymdev@gmail.com>
2024-11-03 23:58:06 +0400

    always use / cgroup path by default

--- scripts/ovpn-run.d/ovpn-run-zapret.sh
+++ scripts/ovpn-run.d/ovpn-run-zapret.sh
@@ -2,13 +2,14 @@
 
 ZAPRET_QNUM="${ZAPRET_QNUM:-200}"
 ZAPRET_FWMARK="${ZAPRET_FWMARK:-0x40000000}"
+ZAPRET_CGROUP_PATH="${ZAPRET_CGROUP_PATH:-/}"
 ZAPRET_MATCH="${ZAPRET_MATCH}"
 if iptables -t mangle -N ZAPRET-"$ZAPRET_QNUM"; then
   iptables -t mangle -I POSTROUTING ! -o lo -j ZAPRET-"$ZAPRET_QNUM"
 fi
 iptables -t mangle -F ZAPRET-"$ZAPRET_QNUM"
 iptables -t mangle -A ZAPRET-"$ZAPRET_QNUM" \
-  -m cgroup --path "$(grep '0::' /proc/$$/cgroup | cut -f 3- -d:)" \
+  -m cgroup --path "$ZAPRET_CGROUP_PATH" \
   -m mark ! --mark "$ZAPRET_FWMARK"/"$ZAPRET_FWMARK" \
   ${ZAPRET_MATCH} \
   -j NFQUEUE --queue-num "$ZAPRET_QNUM"
